fd397e9c745ae0a20c5016afdbbce2181bf0f962
Jabir MDN <88271332+mdnjabir@users.noreply.github.com>
2025-07-14 22:13:00 +0530

    Update sqlcases.sql

--- sqlcases.sql
+++ sqlcases.sql
@@ -18,3 +18,13 @@ ON dem.employee_id = per.employee_id
 WHERE per.attendance>22 
 AND per.tasks_assigned = per.tasks_completed
 ;
+
+-- Top Efficient Employees (sqlcase02)
+
+SELECT d.first_name, d.last_name, d.department, 
+round(p.tasks_completed*100/p.tasks_assigned) as completion_rate
+FROM employee_demographics as d
+JOIN employee_database.performance as p
+ON d.employee_id = p.employee_id
+order by completion_rate desc
+;
